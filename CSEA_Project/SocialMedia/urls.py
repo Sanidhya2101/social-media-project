c123865813aa7e37cd1bfd6cf8a9e6f84220929c
Anjali Priya <anjali.priya848@gmail.com>
2020-12-12 11:37:26 +0530

    added login part

--- CSEA_Project/SocialMedia/urls.py
+++ CSEA_Project/SocialMedia/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path 
+from django.urls import path,include
 from SocialMedia.core import views
 
 urlpatterns = [
-    path('',views.sample,name="Sample"),
+    path('',views.home,name='home'),
+    path('accounts/',include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
 ]
